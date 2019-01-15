b0c084000bc3d7b2045278b4dfb1d4f112cc7d15
awaesome <awe3006@gmail.com>
2019-01-15 21:52:37 +0200

    correcting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const fs = require('fs')
 
-app.use(express.static('../public'))
+app.use(express.static('./build'))
 
 let users = []
 let todos = []
